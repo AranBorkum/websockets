121b2a60299abcb498d8442acec93194ae42c101
Humberto Politi <humberto.politi@mpb.com>
2023-09-13 09:55:38 +0100

    add singal receiver

--- chat/consumers.py
+++ chat/consumers.py
@@ -8,6 +8,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         transaction_id = str(self.scope["url_route"]["kwargs"]["transaction_id"])
+        print(f"connected to {transaction_id}")
         self.room_name = transaction_id
         await self.channel_layer.group_add(
             transaction_id,
@@ -43,3 +44,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def from_button(self, event):
         await self.send(text_data=json.dumps({"message": "from_button"}))
+
+    async def transaction_updated(self, event):
+        print("called transaction_updated")
+        await self.send(text_data=json.dumps({"message": "from model update"}))
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+from asgiref.sync import async_to_sync
 
-# Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from channels.layers import get_channel_layer
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class TransactionModel(models.Model):
+    tx_id = models.CharField(max_length=10)
+
+
+def update_clients(tx):
+    channel_layer = get_channel_layer()
+    print(tx.tx_id)
+    async_to_sync(channel_layer.group_send)(
+        '123',
+        {"type": "transaction_updated", "payload": "heyyy"},
+    )
+
+@receiver(post_save, sender=TransactionModel, dispatch_uid='update_tx_listeners')
+def update_job_status_listeners(sender, instance, **kwargs):
+    '''
+    Sends job status to the browser when a TransactionModel is modified
+    '''
+    print("POST SAVE SIGNAL RECEIVED")
+    update_clients(instance)
